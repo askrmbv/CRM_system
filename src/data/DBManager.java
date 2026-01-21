fd702c425d7293ae28ae4fb7eff22037a922d617
asana <asanali1903@gmail.com>
2026-01-21 18:37:37 +0500

    recreating db man

--- src/data/DBManager.java
+++ src/data/DBManager.java
@@ -4,10 +4,13 @@ import java.sql.DriverManager;
 
 public class DBManager {
     public Connection getConnection() {
+        String url = "jdbc:postgresql://localhost:5432/crm_db";
+        String user = "postgres";
+        String pass = "0000";
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/crm_db", "postgres", "password");
+            return DriverManager.getConnection(url, user, pass);
         } catch (Exception e) {
-            System.out.println("DB Connection Error: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
             return null;
         }
     }
