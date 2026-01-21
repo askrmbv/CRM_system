61869a45530375334fa04e1c2c9c79e699b5ad8f
Damir <252479@astanait.edu.kz>
2026-01-21 16:50:55 +0500

    create Client model with basic fields

--- src/models/Client.java
+++ src/models/Client.java
@@ -1,1 +1,21 @@
 package models;
+
+public class Client {
+    private int id;
+    private String name;
+    private String email;
+    private int status;
+    private double price;
+    public Client(int id, String name, String email, int status, double price) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.status = status;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public int getStatus() { return status; }
+    public double getPrice() { return price; }
+}
