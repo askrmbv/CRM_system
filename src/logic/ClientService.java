8b0f43cff3dbe1dc68afc275a49ecd4a3ce5cf8c
Damir <252479@astanait.edu.kz>
2026-01-21 18:18:48 +0500

    refactor: add model setters

--- src/logic/ClientService.java
+++
@@ -1,2 +0,0 @@
-module CRM.system {
-}
--- src/models/Client.java
+++ src/models/Client.java
@@ -1,21 +1,4 @@
-package models;
-
-public class Client {
-    private int id;
-    private String name;
-    private String email;
-    private int status;
-    private double price;
-    public Client(int id, String name, String email, int status, double price) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.status = status;
-        this.price = price;
-    }
-
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public int getStatus() { return status; }
-    public double getPrice() { return price; }
-}
+public void setName(String name) { this.name = name; }
+public void setEmail(String email) { this.email = email; }
+public void setStatus(int status) { this.status = status; }
+public void setPrice(double price) { this.price = price; }
