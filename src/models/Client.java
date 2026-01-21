fca4b8c4725e3bf6620ade0ff545f08f83f4f84e
Damir <252479@astanait.edu.kz>
2026-01-21 18:22:47 +0500

    style: add toString to model

--- src/models/Client.java
+++ src/models/Client.java
@@ -2,3 +2,10 @@ public void setName(String name) { this.name = name; }
 public void setEmail(String email) { this.email = email; }
 public void setStatus(int status) { this.status = status; }
 public void setPrice(double price) { this.price = price; }
+@Override
+public String toString() {
+    return "Client{name='" + name + "', price=" + price + "}";
+}@Override
+public String toString() {
+    return "Client{name='" + name + "', price=" + price + "}";
+}
