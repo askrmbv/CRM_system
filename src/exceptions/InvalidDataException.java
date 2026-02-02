4bd686e19419daa38f369c4b60a060b41a197186
Damir <252479@astanait.edu.kz>
2026-02-02 11:45:31 +0500

    adding some info

--- src/exceptions/InvalidDataException.java
+++ src/exceptions/InvalidDataException.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+// Exception for data validation errors
 public class InvalidDataException extends RuntimeException {
     public InvalidDataException(String message) {
         super(message);
