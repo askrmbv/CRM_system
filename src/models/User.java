3ad3c893d32a61cd497c553b85bc8d6f8e61d7bf
amangeldinaaiym1-png <amangeldinaaiym1@gmail.com>
2026-02-02 11:08:52 +0500

    adding user functions

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,24 @@
 package models;
 
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public User(int id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public Role getRole() { return role; }
+
+    @Override
+    public String toString() {
+        return String.format("User: %s [%s]", username, role);
+    }
 }
