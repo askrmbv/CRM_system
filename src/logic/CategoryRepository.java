eb5ec0c5778718f0180e826ba79afefc6f971cf5
amangeldinaaiym1-png <amangeldinaaiym1@gmail.com>
2026-02-02 11:25:42 +0500

    adding CategoryRepository functions

--- src/logic/CategoryRepository.java
+++ src/logic/CategoryRepository.java
@@ -1,4 +1,28 @@
 package logic;
 
+import data.DBManager;
+import models.Category;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryRepository {
+    private final DBManager db = DBManager.getInstance();
+
+    public List<Category> getAll() {
+        String sql = "SELECT * FROM categories ORDER BY id ASC";
+        List<Category> list = new ArrayList<>();
+
+        try (Connection conn = db.getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                list.add(new Category(rs.getInt("id"), rs.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
