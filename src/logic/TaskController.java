6c07bd50a88f3a0edf7e080007e904669c1844fd
amangeldinaaiym1-png <amangeldinaaiym1@gmail.com>
2026-02-02 11:19:28 +0500

    adding TaskController functions

--- src/logic/TaskController.java
+++ src/logic/TaskController.java
@@ -1,4 +1,29 @@
 package logic;
 
+import models.Task;
+import java.util.List;
+
 public class TaskController {
+    private final ITaskRepository repo;
+
+    public TaskController(ITaskRepository repo) {
+        this.repo = repo;
+    }
+
+    public boolean addTask(String name, int categoryId) {
+        Task task = new Task(0, name, categoryId);
+        return repo.save(task);
+    }
+
+    public List<Task> getAll() {
+        return repo.getAll();
+    }
+
+    public boolean updateTask(int id, String name, int categoryId) {
+        return repo.update(id, name, categoryId);
+    }
+
+    public boolean deleteTask(int id) {
+        return repo.delete(id);
+    }
 }
