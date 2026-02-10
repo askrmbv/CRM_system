9f38e9467053b95a7b23d0b9b4795c7e8ffb2b8c
asana <asanali1903@gmail.com>
2026-02-10 13:00:59 +0500

    fixing main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-import data.DBManager;
-import repository.CategoryRepository;
 import ui.MyApplication;
 
-void main() {
-    DBManager db = DBManager.getInstance();
-    CategoryRepository categoryRepository = new CategoryRepository(db);
-
-
-    MyApplication app = new MyApplication();
-    app.start();
+public class Main {
+    public static void main(String[] args) {
+        MyApplication app = new MyApplication();
+        app.start();
+    }
 }
