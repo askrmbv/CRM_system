673591e043404fa0401e6cccc85d063ac0443aec
asana <asanali1903@gmail.com>
2026-02-11 19:11:38 +0500

    deleting some ...

--- src/models/Role.java
+++ src/models/Role.java
@@ -3,6 +3,5 @@ package models;
 // User roles
 public enum Role {
     ADMIN,    // Full access to everything
-    MANAGER,  // Can add and edit, but not delete
     EDITOR    // View and edit only
 }
