448e2bb858fd89a018a1478e88180a9ada0c1053
Damir <252479@astanait.edu.kz>
2026-02-11 13:12:00 +0500

    interface for repo

--- src/repository/interfaces/INoteRepository.java
+++ src/repository/interfaces/INoteRepository.java
@@ -1,4 +1,10 @@
 package repository.interfaces;
 
+import models.Note;
+import java.util.List;
+
 public interface INoteRepository {
+    boolean save(Note note);
+    List<Note> getByCustomerId(int customerId);
+    boolean delete(int noteId);
 }
