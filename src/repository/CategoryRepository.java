c7e2373a7a46f76501b7321469f6979f0076ac1f
amangeldinaaiym1-png <amangeldinaaiym1@gmail.com>
2026-02-11 11:14:10 +0500

    categoryrepo

--- src/repository/CategoryRepository.java
+++ src/repository/CategoryRepository.java
@@ -1,4 +1,54 @@
 package repository;
 
-public class CategoryRepository {
+import data.DBManager;
+import repository.interfaces.ICategoryRepository;
+import models.Category;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryRepository implements ICategoryRepository {
+    private final DBManager db = DBManager.getInstance();
+
+    @Override
+    public List<Category> getAll() {
+        String sql = "SELECT id, name FROM categories ORDER BY id";
+        List<Category> list = new ArrayList<>();
+
+        try (Connection conn = db.getConnection();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                list.add(new Category(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Category getById(int id) {
+        String sql = "SELECT id, name FROM categories WHERE id = ?";
+        try (Connection conn = db.getConnection();
+             PreparedStatement st = conn.prepareStatement(sql)) {
+
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+
+            if (rs.next()) {
+                return new Category(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
